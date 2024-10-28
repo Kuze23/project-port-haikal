f6f60f0f6528a6f0e23be79d256dea7511170470
Kuze23 <cahnyankaay@gmail.com>
2024-10-28 22:07:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiText", {
+    strings: ["Haikal Zaki Firmansyah"],
+    typeSpeed: 100,
+    backSpeed: 80, // Corrected property name
+    backDelay: 1500,
+    loop: true
+})
